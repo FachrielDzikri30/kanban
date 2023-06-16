0e28995ffb8883db870b7b89f40300b3bd8d5ad6
FachrielDzikri30 <ffadhilahdzikri@gmail.com>
2023-06-16 13:17:33 +0700

    install recoil and develop task summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import SideMenuLayout from "./layouts/SideMenuLayout"
+import { RecoilRoot } from "recoil"
+import TaskSummary from './features/tasks/components/TaskSummary'
 
 const router = createBrowserRouter([
   {
@@ -13,34 +15,26 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: (
-          <div style={{ display: 'flex' }}>
-            <h1>Home</h1>
-          </div>
-        )
+        element: <TaskSummary />,
       },
       {
         path: 'task-list',
-        element: (
-          <div style={{ display: 'flex' }}>
-            <h1>Task List</h1>
-          </div>
-        )
+        element: <h1>Task List</h1>
       },
       {
         path: 'task-progress',
-        element: (
-          <div style={{ display: 'flex' }}>
-            <h1>Task Progress</h1>
-          </div>
-        )
+        element: <h1>Task Progress</h1>
       },
     ]
   }
 ])
 
 function App(): JSX.Element {
-  return <RouterProvider router={router} />
+  return (
+    <RecoilRoot>
+      <RouterProvider router={router} />
+    </RecoilRoot>
+  )
 }
 
 export default App
